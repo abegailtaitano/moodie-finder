ee29330e53e8b825980b1223fcf7ee0d09ff4ac8
Abegail Taitano <79725435+abegailtaitano@users.noreply.github.com>
2022-04-04 17:08:24 -0500

    moodie finder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,85 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+
+  const CLIENT_ID = "7195f96ac5444808a620d590a629b89f"
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("")
+  const [searchKey, setSearchKey] = useState("")
+  const [playlists, setPlaylists] = useState([])
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+  }
+
+  setToken(token)
+
+}, [])
+
+
+const logout = () => {
+  setToken("")
+  window.localStorage.removeItem("token")
+}
+
+const searchPlaylists = async (e) => {
+  e.preventDefault()
+  const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+          Authorization: `Bearer ${token}`
+      },
+      params: {
+          q: searchKey,
+          type: "playlist"
+      }
+  })
+
+  setPlaylists(data.playlists.items)
+}
+
+const renderPlaylists = () => {
+  return playlists.map(playlist => (
+      <div key={playlist.id}>
+          {playlist.images.length ? <img width={"100%"} src={playlist.images[0].url} alt=""/> : <div>No Image</div>}
+          {playlist.name}
+      </div>
+  ))
+}
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>MOODIE FINDER</h1>
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
+          Login to Spotify</a>
+          : 
+
+{token ?
+    <form onSubmit={searchPlaylists}>
+        <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+        <button type={"submit"}>Search</button>
+        <button onClick={logout}>Logout</button>
+    </form>
+
+    : <h2>Please login</h2>
+}
+
+{renderPlaylists()}
+        
       </header>
     </div>
   );
